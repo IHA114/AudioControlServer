9842ba4f5276ddee4b462f0d2e3a68b7740e22a2
Koorosh <Mr.Oplus@yahoo.fr>
2018-11-30 22:19:43 +0330

    init

---
+++ AudioControlServer/AudioServer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace AudioControlServer
+{
+    
+    class AudioServer
+    {
+        public enum MediaRequest
+        {
+            VOLUME_UP,
+            VOLUME_DOWN,
+            MUTE_TOGGLE,
+            VOLUME_SET,
+            MEDIA_NEXT , 
+            MEDIA_PREVIOUS,
+            MEDIA_STOP , 
+            MEDIA_PLAY 
+        }
+        public delegate void MediaRequestHandler(MediaRequest request, int value = 0);
+        public event MediaRequestHandler OnMedia;
+        private int mListenPort = 0;
+        private string mKey; 
+        private Task mTask;
+        CancellationTokenSource mCancellationSource = new CancellationTokenSource();
+        CancellationToken mCancellationToken;
+        private UdpClient mUdpClient;
+        public AudioServer(int port,string key)
+        {
+            this.mListenPort = port;
+            mUdpClient = new UdpClient(new IPEndPoint(IPAddress.Any, port));
+            mCancellationToken = mCancellationSource.Token;
+            mKey = key;
+        }
+        private void sendEvent(MediaRequest request, int value = 0)
+        {
+            OnMedia?.Invoke(request, value);
+        }
+        public void StartServer()
+        {
+            mUdpClient.AllowNatTraversal(true);
+            mUdpClient.EnableBroadcast = true;
+            mTask = new Task(async () => {
+                while (!mCancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var UdpResult = await mUdpClient.ReceiveAsync();
+                        byte[] buffer = UdpResult.Buffer;
+                        string data = System.Text.Encoding.UTF8.GetString(buffer);
+                        dynamic mData = JsonConvert.DeserializeObject(data);
+                        string key = mData.Key;
+                        string action = mData.Action;
+                        int Volume = mData.Volume;
+                        if (mKey != key)
+                        {
+
+                            byte[] err_msg = Encoding.UTF8.GetBytes("INVALID_KEY");
+                            await mUdpClient.SendAsync(err_msg, err_msg.Length, UdpResult.RemoteEndPoint);
+                            Console.WriteLine($"INVALID KEY FROM : {UdpResult.RemoteEndPoint.ToString()}");
+
+                            continue;
+                        }
+                        if (action == "VOLUME_UP")
+                        {
+                            sendEvent(MediaRequest.VOLUME_UP);
+                        }
+                        else if (action == "VOLUME_DOWN")
+                        {
+                            sendEvent(MediaRequest.VOLUME_DOWN);
+                        }
+                        else if (action == "SET_VOLUME")
+                        {
+                            sendEvent(MediaRequest.VOLUME_SET, Volume);
+                        }
+                        else if (action == "MUTE_TOGGLE")
+                        {
+                            sendEvent(MediaRequest.MUTE_TOGGLE);
+                        }
+
+
+                        else if (action == "MEDIA_NEXT")
+                        {
+                            sendEvent(MediaRequest.MEDIA_NEXT);
+                        }
+                        else if (action == "MEDIA_PREVIUS")
+                        {
+                            sendEvent(MediaRequest.MEDIA_PREVIOUS);
+                        }
+                        else if (action == "MEDIA_STOP")
+                        {
+                            sendEvent(MediaRequest.MEDIA_STOP);
+                        }
+                        else if (action == "MEDIA_PLAY")
+                        {
+                            sendEvent(MediaRequest.MEDIA_PLAY);
+                        }
+                        byte[] msg = Encoding.UTF8.GetBytes("OK");
+                        await mUdpClient.SendAsync(msg, msg.Length, UdpResult.RemoteEndPoint);
+                    }catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                mUdpClient.Close();
+                Console.WriteLine("Thread Exited");
+            },mCancellationToken);
+            mTask.Start();
+        }
+        public void StopServer()
+        {
+            mCancellationSource.Cancel(); 
+        }
+    }
+}
---
+++ AudioControlServer/FormControl.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace AudioControlServer
+{
+    partial class FormControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormControl));
+            this.PortNumber = new System.Windows.Forms.NumericUpDown();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.trayicon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PortNumber)).BeginInit();
+            this.trayMenu.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PortNumber
+            // 
+            this.PortNumber.Location = new System.Drawing.Point(6, 19);
+            this.PortNumber.Maximum = new decimal(new int[] {
+            65534,
+            0,
+            0,
+            0});
+            this.PortNumber.Minimum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.PortNumber.Name = "PortNumber";
+            this.PortNumber.Size = new System.Drawing.Size(234, 20);
+            this.PortNumber.TabIndex = 0;
+            this.PortNumber.Value = new decimal(new int[] {
+            6009,
+            0,
+            0,
+            0});
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 120);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(246, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save!";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // trayicon
+            // 
+            this.trayicon.ContextMenuStrip = this.trayMenu;
+            this.trayicon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayicon.Icon")));
+            this.trayicon.Text = "Audio Control Server";
+            this.trayicon.Visible = true;
+            // 
+            // trayMenu
+            // 
+            this.trayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.trayMenu.Name = "trayMenu";
+            this.trayMenu.Size = new System.Drawing.Size(117, 48);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PortNumber);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(246, 49);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Listen Port";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtKey);
+            this.groupBox2.Location = new System.Drawing.Point(12, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(246, 47);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Key";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(6, 17);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(234, 20);
+            this.txtKey.TabIndex = 0;
+            // 
+            // FormControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 149);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "FormControl";
+            this.ShowIcon = false;
+            this.Text = "Audio Control Server";
+            this.Load += new System.EventHandler(this.FormControl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PortNumber)).EndInit();
+            this.trayMenu.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown PortNumber;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.NotifyIcon trayicon;
+        private System.Windows.Forms.ContextMenuStrip trayMenu;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtKey;
+    }
+}
+
